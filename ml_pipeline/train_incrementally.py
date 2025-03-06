637b013c0123962c7bc76d0780bfa4760c7add48
McEwenTito <mcewentito@gmail.com>
2025-03-06 09:57:50 +0200

    Refactored. Created seperate file for columns

--- ml_pipeline/train_incrementally.py
+++ ml_pipeline/train_incrementally.py
@@ -5,33 +5,8 @@ from sklearn.linear_model import SGDClassifier
 from sklearn.preprocessing import LabelEncoder
 from sklearn.utils.class_weight import compute_class_weight
 from utils import load_object, save_object, preprocess_chunk
+from columns import categorical_cols, numeric_cols
 
-# Define categorical and numeric columns (must match training script)
-categorical_cols = [
-    'Fwd PSH Flags', 'Bwd PSH Flags', 'Fwd URG Flags', 'Bwd URG Flags',
-    'FIN Flag Count', 'SYN Flag Count', 'RST Flag Count', 'PSH Flag Count',
-    'ACK Flag Count', 'URG Flag Count', 'CWE Flag Count', 'ECE Flag Count'
-]
-
-numeric_cols = [
-    'Destination Port', 'Flow Duration', 'Total Fwd Packets', 'Total Backward Packets',
-    'Total Length of Fwd Packets', 'Total Length of Bwd Packets', 'Fwd Packet Length Max',
-    'Fwd Packet Length Min', 'Fwd Packet Length Mean', 'Fwd Packet Length Std',
-    'Bwd Packet Length Max', 'Bwd Packet Length Min', 'Bwd Packet Length Mean',
-    'Bwd Packet Length Std', 'Flow Bytes/s', 'Flow Packets/s', 'Flow IAT Mean',
-    'Flow IAT Std', 'Flow IAT Max', 'Flow IAT Min', 'Fwd IAT Total', 'Fwd IAT Mean',
-    'Fwd IAT Std', 'Fwd IAT Max', 'Fwd IAT Min', 'Bwd IAT Total', 'Bwd IAT Mean',
-    'Bwd IAT Std', 'Bwd IAT Max', 'Bwd IAT Min', 'Fwd Header Length', 'Bwd Header Length',
-    'Fwd Packets/s', 'Bwd Packets/s', 'Min Packet Length', 'Max Packet Length',
-    'Packet Length Mean', 'Packet Length Std', 'Packet Length Variance', 'Down/Up Ratio',
-    'Average Packet Size', 'Avg Fwd Segment Size', 'Avg Bwd Segment Size',
-    'Fwd Avg Bytes/Bulk', 'Fwd Avg Packets/Bulk', 'Fwd Avg Bulk Rate',
-    'Bwd Avg Bytes/Bulk', 'Bwd Avg Packets/Bulk', 'Bwd Avg Bulk Rate',
-    'Subflow Fwd Packets', 'Subflow Fwd Bytes', 'Subflow Bwd Packets',
-    'Subflow Bwd Bytes', 'Init_Win_bytes_forward', 'Init_Win_bytes_backward',
-    'act_data_pkt_fwd', 'min_seg_size_forward', 'Active Mean', 'Active Std',
-    'Active Max', 'Active Min', 'Idle Mean', 'Idle Std', 'Idle Max', 'Idle Min'
-]
 
 data_dir = 'data'
 train_files = [os.path.join(data_dir, filename) for filename in os.listdir(data_dir) if filename.endswith('.csv')]
